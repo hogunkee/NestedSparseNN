e0ac29eedca1066cec67fc959a8fe815411e2f2b
hogunkee <hogunhogun@naver.com>
2018-03-07 22:06:51 +0900

    resnet

--- resnet/resnet.py
+++ resnet/resnet.py
@@ -16,30 +16,39 @@ def make_Wb_tuple(dim1, dim2, tag):
     b = make_bias('bias-' + tag, [dim2])
     return w, b
 
-def make_Wb_list(dim1, dim2, tag, num_layer):
-    W_list = []
-    B_list = []
-    for num in range(num_layer):
-        w = make_variable('Weight' + tag + '-' + str(num), [3, 3, dim1, dim2])
-        b = make_variable('bias' + tag + '-' + str(num), [dim2])
-        W_list.append(w)
-        B_list.append(b)
-        dim1 = dim2
-    return W_list, B_list
-
 ### convolution and maxpooling function ###
-def conv(x,w):
-    return tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME')
+def conv(x, w, stride):
+    return tf.nn.conv2d(x, w, strides=[1,stride,stride,1], padding='SAME')
+
+def conv_bn_relu(x, dim1, dim2, tag, is_training):
+    if dim1 != dim2:
+        stride = 2
+    else:
+        stride = 1
+    w = make_variable('Weight-' + tag, [3, 3, dim1, dim2])
+
+    x_conv = conv(x, w, stride)
+    x_bn = batch_norm(x_conv, dim2, is_training)
+    x_relu = tf.nn.relu(x_bn)
+    return x_relu
+
+def res_block(x, dim1, dim2, scope, tag, is_training):
+    with tf.variable_scope(scope):
+        shortcut = x
+        if dim1 != dim2:
+            w = make_variable('shortcut-' + tag, [3,3,dim1,dim2])
+            shorcut = conv(x, w, 2)
+
+        x_1 = conv_bn_relu(x, dim1, dim2, tag + '-1', is_training)
+        x_2 = conv_bn_relu(x, dim2, dim2, tag + '-2', is_training)
+
+        return tf.add(x_2, x)
 
 def maxpool(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 def conv_maxpool(x, filter_list, bias_list, scope, is_training):
     for i in range(len(filter_list)):
-        '''
-        if not i==0:
-            x = tf.nn.dropout(x, 0.4)
-        '''
         w = filter_list[i]
         b = bias_list[i]
         x = tf.nn.relu(conv(x, w) + b)
@@ -48,28 +57,27 @@ def conv_maxpool(x, filter_list, bias_list, scope, is_training):
     x = maxpool(x)
     return x
 
-def batch_norm(x, n_out, scope, is_training = True):
-    with tf.variable_scope(scope):
-        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
-                name='beta', trainable=True)
-        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
-                name='gamma', trainable=True)
-        batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
-        ema = tf.train.ExponentialMovingAverage(decay = 0.5)
+def batch_norm(x, n_out, is_training = True):
+    beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+            name='beta', trainable=True)
+    gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+            name='gamma', trainable=True)
+    batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+    ema = tf.train.ExponentialMovingAverage(decay = 0.5)
 
-        def mean_var_with_update():
-            ema_apply_op = ema.apply([batch_mean, batch_var])
-            with tf.control_dependencies([ema_apply_op]):
-                return batch_mean, batch_var
+    def mean_var_with_update():
+        ema_apply_op = ema.apply([batch_mean, batch_var])
+        with tf.control_dependencies([ema_apply_op]):
+            return batch_mean, batch_var
 
-        if is_training:
-            mean, var = mean_var_with_update()
-        else:
-            mean, var = batch_mean, batch_var
-            #mean, var = ema.average(batch_mean), ema.average(batch_var)
-            #print(mean,var)
+    if is_training:
+        mean, var = mean_var_with_update()
+    else:
+        mean, var = batch_mean, batch_var
+        #mean, var = ema.average(batch_mean), ema.average(batch_var)
+        #print(mean,var)
 
-        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-5)
+    normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-5)
     return normed
 
 def batch_norm2(x, n_out, scope, is_training = True):
@@ -105,7 +113,6 @@ mean_RGB = [mean_R for i in range(32*32)] + [mean_G for i in range(32*32)] + [me
 class ResNet(object):
     def __init__(self, config, is_training = False):
         self.num_classes = config.num_classes
-        self.keep_prob = 1 - config.dropout
         self.lr = config.learning_rate
         #self.learning_rate = config.learning_rate
         self.beta = config.beta
@@ -115,64 +122,53 @@ class ResNet(object):
         self.num_epoch = config.num_epoch
         self.print_step = config.print_step
         self.is_training = is_training
+        n = self.n = 2
 
 
         self.X = X = tf.placeholder(tf.float32, shape = [None, 3*(self.image_size**2)])
         self.Y = Y = tf.placeholder(tf.float32, shape = [None, self.num_classes])
 
-        '''
-        if is_training == False:
-            self.keep_prob = 1.0
-        '''
-
-        with tf.variable_scope('ResNet'):
-            if not is_training:
-                tf.get_variable_scope().reuse_variables()
-                #print(tf.get_variable_scope().name)
-            self.learning_rate = tf.placeholder(tf.float32, [], name = 'learning_rate')
-            W1, B1 = make_Wb_list(3, 64, '1', 2)
-            W2, B2 = make_Wb_list(64, 128, '2', 2)
-            W3, B3 = make_Wb_list(128, 256, '3', 3)
-            W4, B4 = make_Wb_list(256, 512, '4', 3)
-            W5, B5 = make_Wb_list(512, 512, '5', 3)
-            w_fc1, b_fc1 = make_Wb_tuple(512, 512, 'fc1')
-            w_fc2, b_fc2 = make_Wb_tuple(512, 512, 'fc2')
-            w_fc3, b_fc3 = make_Wb_tuple(512, 10, 'fc3')
-            noise = tf.constant([mean_RGB for i in range(self.batch_size)])
+        noise = tf.constant([mean_RGB for i in range(self.batch_size)])
 
         x = tf.reshape(X, [-1, 32, 32, 3])
         #x = tf.reshape((X - noise), [-1, 32, 32, 3])
+        '''
         x_flip = tf.map_fn(lambda k: tf.image.random_flip_left_right(k), x, dtype = tf.float32)
         paddings = tf.constant([[0,0],[2,2],[2,2],[0,0]])
         x_pad = tf.pad(x_flip, paddings, 'CONSTANT')
         x_padcrop = tf.map_fn(lambda k: tf.random_crop(k, [32,32,3]), x_pad, dtype = tf.float32)
-        #x_padcrop = tf.map_fn(lambda k: tf.random_crop(tf.image.pad_to_bounding_box(k, 2, 2, 36, 36), [32, 32, 3]), x_flip, dtype = tf.float32)
-        #self.sample = x_padcrop[0]
+        '''
         
         # 2개씩 pad & crop
 
-        ## convolution & maxpooling layer ##
-        h1 = conv_maxpool(x_padcrop, W1, B1, '1', self.is_training)
-        #h1 = conv_maxpool(x, W1, B1, '1', self.is_training)
-        h2 = conv_maxpool(h1, W2, B2, '2', self.is_training)
-        h3 = conv_maxpool(h2, W3, B3, '3', self.is_training)
-        h4 = conv_maxpool(h3, W4, B4, '4', self.is_training)
-        h5 = conv_maxpool(h4, W5, B5, '5', self.is_training)
+        dim1 = 16
+        h1 = conv_bn_relu(x, 3, dim1, 'first', self.is_training)
 
-        ## fully connected layer ##
-        h5_flat = tf.reshape(h5, [-1, 512])
-        h_fc1 = tf.nn.relu(tf.matmul(h5_flat, w_fc1) + b_fc1)
-        #h_norm1 = h_fc1
-        h_norm1 = batch_norm2(h_fc1, int(h_fc1.shape[1]), 'fc1', self.is_training)
-        h_dropout1 = tf.nn.dropout(h_norm1, self.keep_prob)
-        h_fc2 = tf.nn.relu(tf.matmul(h_dropout1, w_fc2) + b_fc2)
-        #h_norm2 = h_fc2
-        h_norm2 = batch_norm2(h_fc2, int(h_fc2.shape[1]), 'fc2', self.is_training)
-        h_dropout2 = tf.nn.dropout(h_norm2, self.keep_prob)
-        y = tf.matmul(h_dropout2, w_fc3) + b_fc3
+        h2 = h1
+        for i in range(n):
+            h2 = res_block(h2, dim1, dim1, 'layer1', str(i), self.is_training)
+        
+        h3 = h2
+        dim2 = 32
+        for i in range(n):
+            h3 = res_block(h3, dim1, dim2, 'layer2', str(i), self.is_training)
+            dim1 = dim2
+
+        h4 = h3
+        dim2 = 64 
+        for i in range(n):
+            h4 = res_block(h4, dim1, dim2, 'layer3', str(i), self.is_training)
+            dim1 = dim2
+
+        h_reshape = tf.reshape(h4, [-1, 64, 64])
+        h_max_pool = tf.reduce_max(h_reshape, 1)
+
+        w_fc, b_fc = make_Wb_tuple(64, 10, 'fc')
+        y = tf.matmul(h_max_pool, w_fc) + b_fc
 
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y))
-        regularizer = tf.nn.l2_loss(w_fc1)
+        regularizer = tf.nn.l2_loss(w_fc)
+        '''
         regularizer += tf.nn.l2_loss(w_fc2)
         regularizer += tf.nn.l2_loss(w_fc3)
         for w in W1:
@@ -185,7 +181,7 @@ class ResNet(object):
             regularizer += tf.nn.l2_loss(w)
         for w in W5:
             regularizer += tf.nn.l2_loss(w)
-
+        '''
         self.regularizer = regularizer
         self.loss = loss = loss + self.beta * self.regularizer
 
