a93ea3152648e69684011256e1ff391d76a19b84
hogunkee <hogunhogun@naver.com>
2018-03-20 17:01:19 +0900

    vgg model

--- vgg/vgg.py
+++ vgg/vgg.py
@@ -1,41 +1,5 @@
 import tensorflow as tf
 
-### function to make variable ###
-def make_variable(name, size):
-    return tf.get_variable(name, size, dtype=tf.float32)
-
-def make_Wb_tuple(dim1, dim2, tag):
-    w = make_variable('Weight-' + tag, [dim1, dim2])
-    b = make_variable('bias-' + tag, [dim2])
-    return w, b
-
-def make_Wb_list(dim1, dim2, tag, num_layer):
-    W_list = []
-    B_list = []
-    for num in range(num_layer):
-        w = make_variable('Weight' + tag + '-' + str(num), [3, 3, dim1, dim2])
-        b = make_variable('bias' + tag + '-' + str(num), [dim2])
-        W_list.append(w)
-        B_list.append(b)
-        dim1 = dim2
-    return W_list, B_list
-
-### convolution and maxpooling function ###
-def conv(x,w):
-    return tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME')
-
-def maxpool(x):
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
-
-def conv_maxpool(x, filter_list, bias_list):
-    for i in range(len(filter_list)):
-        w = filter_list[i]
-        b = bias_list[i]
-        x = tf.nn.relu(conv(x, w) + b)
-        #x = tf.nn.batch_normalization(x, 0.0, 0.01, offset = None, scale = None,variance_epsilon = 0.001)
-    x = maxpool(x)
-    return x
-
 ### RGB mean value ###
 mean_R, mean_G, mean_B = 125.3, 122.9, 113.9
 mean_RGB = [mean_R for i in range(32*32)] + [mean_G for i in range(32*32)] + [mean_B for i in range(32*32)]
@@ -69,6 +33,7 @@ class VGG(object):
             self.keep_prob = 1.0
         '''
 
+        '''
         with tf.variable_scope('VGG'):
             if not is_training:
                 tf.get_variable_scope().reuse_variables()
@@ -82,47 +47,65 @@ class VGG(object):
             w_fc2, b_fc2 = make_Wb_tuple(512, 512, 'fc2')
             w_fc3, b_fc3 = make_Wb_tuple(512, 10, 'fc3')
             noise = tf.constant([mean_RGB for i in range(self.batch_size)])
+        '''
 
         x = tf.reshape(X, [-1, self.image_size, self.image_size, self.input_channel])
         #x = tf.reshape(X - noise, [-1, self.image_size, self.image_size, self.input_channel])
         x_flip = tf.map_fn(lambda k: tf.image.random_flip_left_right(k), x, dtype = tf.float32)
-        x_padcrop = tf.map_fn(lambda k: tf.random_crop(tf.image.pad_to_bounding_box(k, 4, 4, 40, 40), [32, 32, 3]), x_flip, dtype = tf.float32)
+        x_padcrop = tf.map_fn(lambda k: tf.random_crop(
+            tf.image.pad_to_bounding_box(k, 4, 4, 40, 40), [32, 32, 3]), 
+            x_flip, dtype = tf.float32)
 
-        ## convolution & maxpooling layer ##
-        h1 = conv_maxpool(x_flip, W1, B1)
-        #h1 = conv_maxpool(x_padcrop, W1, B1)
-        h2 = conv_maxpool(h1, W2, B2)
-        h3 = conv_maxpool(h2, W3, B3)
-        h4 = conv_maxpool(h3, W4, B4)
-        h5 = conv_maxpool(h4, W5, B5)
-
-        ## fully connected layer ##
-        h5_flat = tf.reshape(h5, [-1, 512])
-        h_fc1 = tf.nn.relu(tf.matmul(h5_flat, w_fc1) + b_fc1)
-        #h_norm1 = tf.nn.batch_normalization(h_fc1, 0.0, 0.01, offset = None, scale = None,variance_epsilon = 0.001)
-        h_dropout1 = tf.nn.dropout(h_fc1, self.keep_prob)
-        h_fc2 = tf.nn.relu(tf.matmul(h_dropout1, w_fc2) + b_fc2)
-        #h_norm2 = tf.nn.batch_normalization(h_fc2, 0.0, 0.01, offset = None, scale = None,variance_epsilon = 0.001)
-        h_dropout2 = tf.nn.dropout(h_fc2, self.keep_prob)
-        y = tf.matmul(h_dropout2, w_fc3) + b_fc3
+        #channel: 64
+        if self.padding:
+            h1 = conv(x_padcrop, 64, 'layer-1')
+        else: 
+            h1 = conv(x, 64, 'layer-1')
+        h1 = tf.nn.relu(h1)
+        h2 = conv(h1, 64, 'layer-2')
+        h2 = tf.nn.relu(h2)
+        #channel: 128
+        h3 = maxpool(h2, 'layer-3')
+        h3 = conv(h3, 128, 'layer-3')
+        h3 = tf.nn.relu(h3)
+        h4 = conv(h3, 128, 'layer-4')
+        h4 = tf.nn.relu(h4)
+        #channel: 256
+        h5 = maxpool(h4, 'layer-5')
+        h5 = conv(h5, 256, 'layer-5')
+        h5 = tf.nn.relu(h5)
+        h6 = conv(h6, 256, 'layer-6')
+        h6 = tf.nn.relu(h6)
+        h7 = conv(h7, 256, 'layer-7')
+        h7 = tf.nn.relu(h7)
+        #channel: 512
+        h8 = maxpool(h7, 'layer-8')
+        h8 = conv(h8, 512, 'layer-8')
+        h8 = tf.nn.relu(h8)
+        h9 = conv(h8, 512, 'layer-9')
+        h9 = tf.nn.relu(h9)
+        h10 = conv(h9, 512, 'layer-10')
+        h10 = tf.nn.relu(h10)
+        #channel: 512
+        h11 = maxpool(h10, 'layer-11')
+        h11 = conv(h11, 512, 'layer-11')
+        h11 = tf.nn.relu(h11)
+        h12 = conv(h11, 512, 'layer-12')
+        h12 = tf.nn.relu(h12)
+        h13 = conv(h12, 512, 'layer-13')
+        h13 = tf.nn.relu(h13)
+        #fc layer 512, 512, 10
+        h14 = tf.reshape(h13, [-1, 512])
+        h14 = fc(h14, 512, 'fc-1')
+        h14 = tf.nn.relu(h14)
+        h15 = fc(h14, 512, 'fc-2')
+        h15 = tf.nn.relu(h15)
+        y = fc(h15, 10, 'fc-3')
 
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y))
-        regularizer = tf.nn.l2_loss(w_fc1)
-        regularizer += tf.nn.l2_loss(w_fc2)
-        regularizer += tf.nn.l2_loss(w_fc3)
-        for w in W1:
-            regularizer += tf.nn.l2_loss(w)
-        for w in W2:
-            regularizer += tf.nn.l2_loss(w)
-        for w in W3:
-            regularizer += tf.nn.l2_loss(w)
-        for w in W4:
-            regularizer += tf.nn.l2_loss(w)
-        for w in W5:
-            regularizer += tf.nn.l2_loss(w)
+        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        loss += self.beta * sum(reg_losses)
 
-        self.regularizer = regularizer
-        self.loss = loss = loss + self.beta * self.regularizer
         with tf.variable_scope('VGG'):
             if not is_training:
                 tf.get_variable_scope().reuse_variables()
@@ -132,3 +115,22 @@ class VGG(object):
         correct_predict = tf.equal(tf.argmax(y,1), tf.argmax(Y,1))
         self.accur = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
 
+    ### convolution, maxpooling and fully-connected function ###
+    def conv(self, x, num_out, scope):
+        c_init = tf.truncated_normal_initializer(stddev=5e-2)
+        b_init = tf.constant_initializer(0.0)
+        regularizer = tf.contrib.layers.l2_regularizer(scale=self.beta)
+        return tf.contrib.layers.conv2d(x, num_out, [3,3] activation_fn=None,
+                weights_initializer=c_init, weights_regularizer=regularizer,
+                biases_initializer=b_init, scope=scope)
+
+    def maxpool(self, x, scope):
+        return tf.contrib.layers.max_pool2d(x, [2,2], padding='SAME', scope=scope)
+    
+    def fc(self, x, num_out, scope):
+        f_init = tf.truncated_normal_initializer(stddev=5e-2)
+        b_init = tf.constant_initializer(0.0)
+        regularizer = tf.contrib.layers.l2_regularizer(scale=self.beta)
+        return tf.contrib.layers.fully_connected(x, num_out, activation_fn=None,
+                weights_initializer=f_init, weights_regularizer=regularizer,
+                biases_initializer=b_init, scope=scope)
